1d40511d4b76f525eb18cc4e53246d63f0fd1641
willian <willian@zeus.athena.home>
2025-05-07 12:58:47 -0300

    Ajusta Blueprint e corrige caminho de IA e estilo no template

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/editor/static')
     app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), '..', 'saved_files')
     from . import routes
-    app.register_blueprint(routes.bp)
+    app.register_blueprint(routes.bp, url_prefix='/editor')
     return app
